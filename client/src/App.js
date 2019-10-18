ff3a450b82e8d4f746d2551a04fe80401c039d95
gelinas <jlgelinas@gmail.com>
2019-10-18 11:40:52 -0400

    login functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,24 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import PrivateRoute from "./utils/PrivateRoute";
 import Login from "./components/Login";
+import BubblePage from "./components/BubblePage";
+
 import "./styles.scss";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <Switch>
+          <Route exact path="/" component={Login} />
+          {/* 
+            Build a PrivateRoute component that will 
+            display BubblePage when you're authenticated 
+          */}
+          <PrivateRoute path="/bubbles" component={BubblePage} />
+        </Switch>
       </div>
     </Router>
   );
--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -8,6 +8,9 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  useEffect(() => {
+    
+  })
 
   return (
     <>
